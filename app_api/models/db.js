44e0abe162771c8724257aa6aab1781a354a0a25
AyberkGameDev <109525336+illusionDis@users.noreply.github.com>
2025-12-14 22:13:28 +0300

    Add files via upload

---
+++ app_api/models/db.js
@@ -0,0 +1,23 @@
+var mongoose=require("mongoose");
+//var dbURI="mongodb://localhost/mekanbul";
+//var dbURI = 'mongodb+srv://godmunamei12_db_user:RzzCqcpUD28xoj8p@mekanbul.qfgnxgi.mongodb.net/';
+
+require("dotenv").config();
+var dbURI = process.env.MONGODB_URI;
+
+mongoose.connect(dbURI);
+mongoose.connection.on("connected",function(){
+    console.log("Mongoose "+dbURI+" adresindeki veritabanına bağlandı.");
+});
+mongoose.connection.on("error",function(){
+    console.log("Mongoose bağlantı hatası.");
+});
+mongoose.connection.on("disconnected",function(){
+    console.log("Mongoose bağlantısı kesildi.");
+});
+process.on("SIGINT",function(){
+    mongoose.connection.close();
+    console.log("Mongoose uygulama sonlandırma nedeniyle bağlantıyı kapattı.");
+    process.exit(0);
+});
+require("./venue");
